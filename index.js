c3625cedc385c859645b3e7b479982c43f557786
Kristofer Joseph <kristoferjoseph@gmail.com>
2015-04-29 10:13:37 -0700

    Adds rounding of output to 3 decimal points for browsers

--- index.js
+++ index.js
@@ -18,34 +18,34 @@ var ratioNames = {
     majorEleventh : 2.667,
     majorTwelfth  : 3,
     doubleOctave  : 4
-}
+};
 
 module.exports = function modularscale(options) {
-  options = options        || {}
-  value   = options.value  || 0
-  bases   = options.bases  || msBases
-  ratios  = options.ratios || msRatios
+  options = options        || {};
+  value   = options.value  || 0;
+  bases   = options.bases  || msBases;
+  ratios  = options.ratios || msRatios;
 
   bases = bases.map(function(base) {
     if (typeof base === 'string') {
-      base = parseFloat(base, 10) || 1
+      base = parseFloat(base, 10) || 1;
     }
-    return base
-  })
+    return base;
+  });
 
   ratios = ratios.map(function(ratio) {
     if (typeof ratio === 'string') {
-      ratio = ratioNames[ratio] || parseFloat(ratio, 10) || 0
+      ratio = ratioNames[ratio] || parseFloat(ratio, 10) || 0;
     }
-    return ratio
-  })
+    return ratio;
+  });
 
   return function ms(value) {
-    var r = []
-    var strand = null
-    var ratio
-    var base
-    var i = 0
+    var r = [];
+    var strand = null;
+    var ratio;
+    var base;
+    var i = 0;
 
     for (ratio = 0; ratio < ratios.length; ratio++) {
       for (base = 0; base < bases.length; base++) {
@@ -55,72 +55,76 @@ module.exports = function modularscale(options) {
         // Find values on a positive scale
         if (value >= 0) {
           // Find lower values on the scale
-          i = 0
+          i = 0;
           while((Math.pow(ratios[ratio], i) * bases[base]) >= bases[0]) {
-            r.push([Math.pow(ratios[ratio], i) * bases[base], strand])
-            i--
+            r.push([Math.pow(ratios[ratio], i) * bases[base], strand]);
+            i--;
           }
 
           // Find higher possible values on the scale
-          i = 0
+          i = 0;
           while(Math.pow(ratios[ratio], i) * bases[base] <= Math.pow(ratios[ratio], value + 1) * bases[base]) {
-            r.push([Math.pow(ratios[ratio], i) * bases[base], strand])
-            i++
+            r.push([Math.pow(ratios[ratio], i) * bases[base], strand]);
+            i++;
           }
         } else {
           // Find values on a negative scale
-          i = 0
+          i = 0;
           while((Math.pow(ratios[ratio], i) * bases[base]) <= bases[0]) {
-            r.push([Math.pow(ratios[ratio], i) * bases[base], strand])
-            i++
+            r.push([Math.pow(ratios[ratio], i) * bases[base], strand]);
+            i++;
           }
 
           // // Find higher possible values on the scale
-          i = 0
+          i = 0;
           while((Math.pow(ratios[ratio], i) * bases[base]) >= (Math.pow(ratios[ratio], value - 1) * bases[base])) {
             if (Math.pow(ratios[ratio], i) * bases[base] <= bases[0]) {
-              r.push([Math.pow(ratios[ratio], i) * bases[base], strand])
+              r.push([Math.pow(ratios[ratio], i) * bases[base], strand]);
             }
-            i--
+            i--;
           }
         }
       }
     }
 
-    r = msUnique(r)
+    r = msUnique(r);
 
     // reverse array if value is negative
     if(value < 0) {
-      r = r.reverse()
+      r = r.reverse();
     }
 
-    return r[Math.abs(value)][0]
-  }
-}
+    return round(r[Math.abs(value)][0]);
+  };
+};
 
 function msUnique(origArr) {
-  var x
-  var y
-  var lastVal
-  var i
+  var x;
+  var y;
+  var lastVal;
+  var i;
 
   origArr = origArr.sort(function(a,b) {
-    x = a[0]
-    y = b[0]
-    return x-y
-  })
+    x = a[0];
+    y = b[0];
+    return x-y;
+  });
 
-  newArr = []
-  lastVal = null
+  newArr = [];
+  lastVal = null;
 
   for (i = 0; i < origArr.length; i++) {
     var currentVal = origArr[i][0];
     if (currentVal != lastVal) {
-      newArr.push(origArr[i])
+      newArr.push(origArr[i]);
     }
-    lastVal = currentVal
+    lastVal = currentVal;
   }
 
-  return newArr
+  return newArr;
+}
+
+function round(value) {
+  return Math.round(value * 1000) / 1000;
 }
 
--- test.js
+++ test.js
@@ -1,11 +1,7 @@
 var mocha = require('mocha'),
     expect = require('chai').expect,
     modularScale = require('./'),
-    ms
-
-function round(value) {
-  return Math.round(value * 1000) / 1000
-}
+    ms;
 
 describe('Modular Scale', function() {
 
@@ -13,49 +9,50 @@ describe('Modular Scale', function() {
     ms = modularScale({
       ratios: ['majorTwelfth'],
       bases: [1]
-    })
-    expect(ms(3)).to.equal(27)
+    });
+    expect(ms(3)).to.equal(27);
   });
 
   it('should accept bases with units', function() {
     ms = modularScale({
       ratios: ['majorTwelfth'],
       bases: ['1em']
-    })
-    expect(ms(3)).to.equal(27)
+    });
+    expect(ms(3)).to.equal(27);
   });
 
   it('should use accept ratio numbers as strings', function() {
     ms = modularScale({
       ratios: ['2.667'],
       bases: [1]
-    })
-    expect(round(ms(3))).to.equal(18.97)
+    });
+    expect(ms(3)).to.equal(18.97);
   });
 
   it('should return octave scale with multiple bases', function() {
     ms = modularScale({
       ratios: [2],
       bases: ['16',12]
-    })
+    });
 
-    expect(ms(-1)).to.equal(12)
-    expect(ms(0)).to.equal(12)
-    expect(ms('1')).to.equal(16)
-    expect(ms(2)).to.equal(24)
-    expect(ms(3)).to.equal(32)
-    expect(ms('4')).to.equal(48)
+    expect(ms(-1)).to.equal(12);
+    expect(ms(0)).to.equal(12);
+    expect(ms('1')).to.equal(16);
+    expect(ms(2)).to.equal(24);
+    expect(ms(3)).to.equal(32);
+    expect(ms('4')).to.equal(48);
   });
 
   it('should return default scale', function() {
-    ms = modularScale()
+    ms = modularScale();
 
-    expect(round(ms(-1))).to.equal(0.618)
-    expect(round(ms(0))).to.equal(1)
-    expect(round(ms(1))).to.equal(1.618)
-    expect(round(ms(2))).to.equal(2.618)
-    expect(round(ms(3))).to.equal(4.236)
-    expect(round(ms(4))).to.equal(6.854)
+    expect(ms(-1)).to.equal(0.618);
+    expect(ms(0)).to.equal(1);
+    expect(ms(1)).to.equal(1.618);
+    expect(ms(2)).to.equal(2.618);
+    expect(ms(3)).to.equal(4.236);
+    expect(ms(4)).to.equal(6.854);
   });
 
 });
+
